7d046583158b0210a0f35069f94c5ac990d94bbe
Maxter315 <Maxter315@gmail.com>
2016-09-11 22:23:19 +0300

    added firmware
    First demo version of the sensor

---
+++ demo_old/D1_http.ino
@@ -0,0 +1,531 @@
+#define LUM_PIN 0
+#define MQ_APIN 3
+
+#include <MQ135.h>
+#include <TFT_HX8357.h> // Hardware-specific library
+#include <SFE_BMP180.h>
+#include <Wire.h>
+
+SFE_BMP180 pressure;
+TFT_HX8357 tft = TFT_HX8357();       // Invoke custom library
+int measurePin = 7;
+int ledPower = 12;
+
+float getDust(void);
+//==============================================================================
+
+#define LTBLUE    0xB6DF
+#define LTTEAL    0xBF5F
+#define LTGREEN   0xBFF7
+#define LTCYAN    0xC7FF
+#define LTRED     0xFD34
+#define LTMAGENTA 0xFD5F
+#define LTYELLOW  0xFFF8
+#define LTORANGE  0xFE73
+#define LTPINK    0xFDDF
+#define LTPURPLE  0xCCFF
+#define LTGREY    0xE71C
+
+#define BLUE      0x001F
+#define TEAL      0x0438
+#define GREEN     0x07E0
+#define CYAN      0x07FF
+#define RED       0xF800
+#define MAGENTA   0xF81F
+#define YELLOW    0xFFE0
+#define ORANGE    0xFC00
+#define PINK      0xF81F
+#define PURPLE    0x8010
+#define GREY      0xC618
+#define WHITE     0xFFFF
+#define BLACK     0x0000
+
+#define DKBLUE    0x000D
+#define DKTEAL    0x020C
+#define DKGREEN   0x03E0
+#define DKCYAN    0x03EF
+#define DKRED     0x6000
+#define DKMAGENTA 0x8008
+#define DKYELLOW  0x8400
+#define DKORANGE  0x8200
+#define DKPINK    0x9009
+#define DKPURPLE  0x4010
+#define DKGREY    0x4A49
+
+// these are the only external variables used by the graph function
+// it's a flag to draw the coordinate system only on the first call to the Graph() function
+// and will mimize flicker
+// also create some variables to store the old x and y, if you draw 2 graphs on the same display
+// you will need to store ox and oy per each display
+boolean display1 = true;
+boolean update1 = true;
+
+double ox = -999, oy = -999; // Force them to be off screen
+
+/*
+  function to draw a cartesian coordinate system and plot whatever data you want
+  just pass x and y and the graph will be drawn
+
+  huge arguement list
+  &d name of your display object
+  x = x data point
+  y = y datapont
+  gx = x graph location (lower left)
+  gy = y graph location (lower left)
+  w = width of graph
+  h = height of graph
+  xlo = lower bound of x axis
+  xhi = upper bound of x asis
+  xinc = division of x axis (distance not count)
+  ylo = lower bound of y axis
+  yhi = upper bound of y asis
+  yinc = division of y axis (distance not count)
+  title = title of graph
+  xlabel = x asis label
+  ylabel = y asis label
+  &redraw = flag to redraw graph on first call only
+  color = plotted trace colour
+*/
+
+
+void Graph(TFT_HX8357 &tft, double x, double y, byte dp,
+                           double gx, double gy, double w, double h,
+                           double xlo, double xhi, double xinc,
+                           double ylo, double yhi, double yinc,
+                           char *title, char *xlabel, char *ylabel,
+                           boolean &redraw, unsigned int color) {
+
+  double ydiv, xdiv;
+  double i;
+  double temp;
+  int rot, newrot;
+
+  // gcolor = graph grid colors
+  // acolor = axes line colors
+  // pcolor = color of your plotted data
+  // tcolor = text color
+  // bcolor = background color
+  unsigned int gcolor = DKBLUE;
+  unsigned int acolor = RED;
+  unsigned int pcolor = color;
+  unsigned int tcolor = WHITE;
+  unsigned int bcolor = BLACK;
+
+  if (redraw == true) {
+
+    redraw = false;
+    // initialize old x and old y in order to draw the first point of the graph
+    // but save the transformed value
+    // note my transform funcition is the same as the map function, except the map uses long and we need doubles
+    //ox = (x - xlo) * ( w) / (xhi - xlo) + gx;
+    //oy = (y - ylo) * (gy - h - gy) / (yhi - ylo) + gy;
+
+    tft.setTextDatum(MR_DATUM);
+
+    // draw y scale
+    for ( i = ylo; i <= yhi; i += yinc) {
+      // compute the transform
+      temp =  (i - ylo) * (gy - h - gy) / (yhi - ylo) + gy;
+
+      if (i == 0) {
+        tft.drawLine(gx, temp, gx + w, temp, acolor);
+        tft.setTextColor(acolor, bcolor);
+        tft.drawString(xlabel, (int)(gx + w) , (int)temp, 2);
+      }
+      else {
+        tft.drawLine(gx, temp, gx + w, temp, gcolor);
+      }
+      // draw the axis labels
+      tft.setTextColor(tcolor, bcolor);
+      // precision is default Arduino--this could really use some format control
+      tft.drawFloat(i, dp, gx - 4, temp, 1);
+    }
+
+    // draw x scale
+    for (i = xlo; i <= xhi; i += xinc) {
+
+      // compute the transform
+      temp =  (i - xlo) * ( w) / (xhi - xlo) + gx;
+      if (i == 0) {
+        tft.drawLine(temp, gy, temp, gy - h, acolor);
+        tft.setTextColor(acolor, bcolor);
+        tft.setTextDatum(BC_DATUM);
+        tft.drawString(ylabel, (int)temp, (int)(gy - h - 8) , 2);
+      }
+      else {
+        tft.drawLine(temp, gy, temp, gy - h, gcolor);
+      }
+      // draw the axis labels
+      tft.setTextColor(tcolor, bcolor);
+      tft.setTextDatum(TC_DATUM);
+      // precision is default Arduino--this could really use some format control
+      tft.drawFloat(i, dp, temp, gy + 7, 1);
+    }
+
+    //now draw the graph labels
+    tft.setTextColor(tcolor, bcolor);
+    tft.drawString(title, (int)(gx + w / 2) , (int)(gy - h - 30), 4);
+  }
+
+  // the coordinates are now drawn, plot the data
+  // the entire plotting code are these few lines...
+  // recall that ox and oy are initialized above
+  //x =  (x - xlo) * ( w) / (xhi - xlo) + gx;
+  //y =  (y - ylo) * (gy - h - gy) / (yhi - ylo) + gy;
+  //tft.drawLine(ox, oy, x, y, pcolor);
+  // it's up to you but drawing 2 more lines to give the graph some thickness
+  //tft.drawLine(ox, oy + 1, x, y + 1, pcolor);
+  //tft.drawLine(ox, oy - 1, x, y - 1, pcolor);
+  //ox = x;
+  //oy = y;
+
+}
+
+void Trace(TFT_HX8357 &tft, double x,  double y,  byte dp,
+           double gx, double gy,
+           double w, double h,
+           double xlo, double xhi, double xinc,
+           double ylo, double yhi, double yinc,
+           char *title, char *xlabel, char *ylabel,
+           boolean &update1, unsigned int color)
+{
+  double ydiv, xdiv;
+  double i;
+  double temp;
+  int rot, newrot;
+
+  //unsigned int gcolor = DKBLUE;   // gcolor = graph grid color
+  unsigned int acolor = RED;        // acolor = main axes and label color
+  unsigned int pcolor = color;      // pcolor = color of your plotted data
+  unsigned int tcolor = WHITE;      // tcolor = text color
+  unsigned int bcolor = BLACK;      // bcolor = background color
+
+  // initialize old x and old y in order to draw the first point of the graph
+  // but save the transformed value
+  // note my transform funcition is the same as the map function, except the map uses long and we need doubles
+  if (update1) {
+    update1 = false;
+    
+    ox = (x - xlo) * ( w) / (xhi - xlo) + gx;
+    oy = (y - ylo) * (gy - h - gy) / (yhi - ylo) + gy;
+
+    if ((ox < gx) || (ox > gx+w)) {update1 = true; return;}
+    if ((oy < gy-h) || (oy > gy)) {update1 = true; return;}
+    
+
+    tft.setTextDatum(MR_DATUM);
+
+    // draw y scale
+    for ( i = ylo; i <= yhi; i += yinc) {
+      // compute the transform
+      temp =  (i - ylo) * (gy - h - gy) / (yhi - ylo) + gy;
+
+      if (i == 0) {
+        tft.setTextColor(acolor, bcolor);
+        tft.drawString(xlabel, (int)(gx + w) , (int)temp, 2);
+      }
+      // draw the axis labels
+      tft.setTextColor(tcolor, bcolor);
+      // precision is default Arduino--this could really use some format control
+      tft.drawFloat(i, dp, gx - 4, temp, 1);
+    }
+
+    // draw x scale
+    for (i = xlo; i <= xhi; i += xinc) {
+
+      // compute the transform
+      temp =  (i - xlo) * ( w) / (xhi - xlo) + gx;
+      if (i == 0) {
+        tft.setTextColor(acolor, bcolor);
+        tft.setTextDatum(BC_DATUM);
+        tft.drawString(ylabel, (int)temp, (int)(gy - h - 8) , 2);
+      }
+
+      // draw the axis labels
+      tft.setTextColor(tcolor, bcolor);
+      tft.setTextDatum(TC_DATUM);
+      // precision is default Arduino--this could really use some format control
+      tft.drawFloat(i, dp, temp, gy + 7, 1);
+    }
+
+    //now draw the graph labels
+    tft.setTextColor(tcolor, bcolor);
+    tft.drawString(title, (int)(gx + w / 2) , (int)(gy - h - 30), 4);
+  }
+
+  // the coordinates are now drawn, plot the data
+  // the entire plotting code are these few lines...
+  // recall that ox and oy are initialized above
+  x =  (x - xlo) * ( w) / (xhi - xlo) + gx;
+  y =  (y - ylo) * (gy - h - gy) / (yhi - ylo) + gy;
+
+  if ((x < gx) || (x > gx+w)) {update1 = true; return;}
+  if ((y < gy-h) || (y > gy)) {update1 = true; return;}
+    
+    
+  tft.drawLine(ox, oy, x, y, pcolor);
+  // it's up to you but drawing 2 more lines to give the graph some thickness
+  //tft.drawLine(ox, oy + 1, x, y + 1, pcolor);
+  //tft.drawLine(ox, oy - 1, x, y - 1, pcolor);
+  ox = x;
+  oy = y;
+
+}
+
+//==============================================================================
+
+MQ135 gasSensor = MQ135(MQ_APIN);
+float CO2ppm = 0;
+float lum = 0;
+#define asize 600
+float data[asize];
+
+void setup() {
+  Serial.begin(9600);
+  double x, y;
+  
+  pinMode(ledPower,OUTPUT);
+  
+  tft.begin();
+  pressure.begin();
+  
+  tft.fillScreen(BLACK);
+  tft.setRotation(1);
+
+  //preheating delay
+  char heattxt[15]="Preheating";
+  char dots[15]="             \0";
+  tft.drawString(heattxt,10,10,3);
+  for (int i=0;i<10;i++){
+    dots[i]='.';
+    tft.drawString(dots,40,40,3);
+    delay(1000);
+  }
+  tft.fillScreen(BLACK);
+/*
+  Graph(tft, x, y, 1, 60, 290, 390, 260, 0, 300, 60, 0, 2000, 250, "", "", "", display1, YELLOW);
+
+  //delay(1000);
+  
+  update1 = true;
+  
+  for (x = 0; x <= 300; x+=1) {
+    CO2ppm = gasSensor.getPPM();
+    Trace(tft, x, CO2ppm, 1, 60, 290, 390, 260, 0, 300, 60, 0, 2000, 250, "CO2", "time", "ppm", update1, YELLOW);
+  delay(500);
+  }
+*/
+
+}
+
+int rfr=0;
+ 
+void loop() {
+  double x, y;
+  //CO2ppm = gasSensor.getPPM();
+  lum = analogRead(LUM_PIN)/2.0;
+  float rzero = gasSensor.getRZero();
+  float maxv=0;
+
+  CO2ppm = gasSensor.getPPM();
+  data[asize-1] = CO2ppm;
+    
+  for(int i=0;i<asize;i++){
+    if(data[i]>=maxv){maxv=data[i];}
+  }
+  
+  int maxv2 = (int)maxv;
+  //maxv2=2000;
+  if (rfr%4 == 0){
+  tft.fillScreen(BLACK);
+  tft.setRotation(1);
+  Graph(tft, x, y, 1, 60, 290, 390, 260, 0, asize, 50, 0, 1.2*maxv2, maxv2/4, "", "", "", display1, YELLOW);
+    update1 = true;
+  for (x = 0; x < asize; x+=1) {
+    float co = data[(int)x];
+    Trace(tft, x, co, 1, 60, 290, 390, 260, 0, asize, 50, 0, 1.2*maxv2, maxv2/4, "CO2", "time", "ppm", update1, YELLOW);
+  }
+  //if(rfr%8 == 0){Serial.println(CO2ppm);}
+
+float pres = 0, tempr = 0;
+
+char status;
+double T,P,p0,a;
+
+  status = pressure.startTemperature();
+  if (status != 0)
+  {
+    // Wait for the measurement to complete:
+    delay(status);
+
+    // Retrieve the completed temperature measurement:
+    // Note that the measurement is stored in the variable T.
+    // Function returns 1 if successful, 0 if failure.
+
+    status = pressure.getTemperature(T);
+    if (status != 0)
+    {
+      // Print out the measurement:
+      //Serial.print("temperature: ");
+      //Serial.print(T,2);
+      //Serial.print(" deg C, ");
+      
+      // Start a pressure measurement:
+      // The parameter is the oversampling setting, from 0 to 3 (highest res, longest wait).
+      // If request is successful, the number of ms to wait is returned.
+      // If request is unsuccessful, 0 is returned.
+
+      status = pressure.startPressure(3);
+      if (status != 0)
+      {
+        // Wait for the measurement to complete:
+        delay(status);
+
+        // Retrieve the completed pressure measurement:
+        // Note that the measurement is stored in the variable P.
+        // Note also that the function requires the previous temperature measurement (T).
+        // (If temperature is stable, you can do one temperature measurement for a number of pressure measurements.)
+        // Function returns 1 if successful, 0 if failure.
+
+        status = pressure.getPressure(P,T);
+        if (status != 0)
+        {
+          // Print out the measurement:
+          //Serial.print("absolute pressure: ");
+          //Serial.print(P,2);
+          ///Serial.print(" mb, ");
+
+          // The pressure sensor returns abolute pressure, which varies with altitude.
+          // To remove the effects of altitude, use the sealevel function and your current altitude.
+          // This number is commonly used in weather reports.
+          // Parameters: P = absolute pressure in mb, ALTITUDE = current altitude in m.
+          // Result: p0 = sea-level compensated pressure in mb
+
+        }
+        //else Serial.println("error retrieving pressure measurement\n");
+      }
+      //else Serial.println("error starting pressure measurement\n");
+    }
+    //else Serial.println("error retrieving temperature measurement\n");
+  }
+
+
+
+  //pres = getPressure();
+  float dust = getDust();
+  //int dd=analogRead(7);
+  tft.setTextColor(TFT_CYAN);
+  tft.drawString("Air pressure, mmHg:", 360, 5,2);
+  tft.drawNumber(P*0.750062, 450, 5,2);
+  tft.drawString("Temperature, deg C:", 360, 20,2);
+  tft.drawNumber(T, 450, 20,2);
+  tft.drawString("Dust:", 360, 35,2);
+  tft.drawFloat(dust,3, 450, 35,2);
+  
+  Serial.print("<b>CO2: </b>");
+  Serial.print(CO2ppm);
+  Serial.print(" ppm<br><b>Temperature: </b>");
+  Serial.print(T);
+  Serial.print(" degC<br><b>Pressure: </b>");
+  Serial.print(P*0.750062);
+  Serial.print(" mmhg\r");
+}
+rfr++;
+
+  for(int i=0;i<(asize-1);i++){
+    data[i]=data[i+1];
+  }
+
+/*
+  Serial.print("Luminance: ");
+  Serial.print(lum);
+  Serial.print("\tCO2, ppm: ");
+  Serial.println(CO2ppm);
+  */
+  //Serial.print(rzero);
+
+  delay(500);
+
+}
+
+double getPressure()
+{
+  char status;
+  double T,P,p0,a;
+
+  // You must first get a temperature measurement to perform a pressure reading.
+  
+  // Start a temperature measurement:
+  // If request is successful, the number of ms to wait is returned.
+  // If request is unsuccessful, 0 is returned.
+
+  status = pressure.startTemperature();
+  if (status != 0)
+  {
+    // Wait for the measurement to complete:
+
+    delay(status);
+
+    // Retrieve the completed temperature measurement:
+    // Note that the measurement is stored in the variable T.
+    // Use '&T' to provide the address of T to the function.
+    // Function returns 1 if successful, 0 if failure.
+
+    status = pressure.getTemperature(T);
+    if (status != 0)
+    {
+      // Start a pressure measurement:
+      // The parameter is the oversampling setting, from 0 to 3 (highest res, longest wait).
+      // If request is successful, the number of ms to wait is returned.
+      // If request is unsuccessful, 0 is returned.
+
+      status = pressure.startPressure(3);
+      if (status != 0)
+      {
+        // Wait for the measurement to complete:
+        delay(status);
+
+        // Retrieve the completed pressure measurement:
+        // Note that the measurement is stored in the variable P.
+        // Use '&P' to provide the address of P.
+        // Note also that the function requires the previous temperature measurement (T).
+        // (If temperature is stable, you can do one temperature measurement for a number of pressure measurements.)
+        // Function returns 1 if successful, 0 if failure.
+
+        status = pressure.getPressure(P,T);
+        if (status != 0)
+        {
+          return(P);
+        }
+        else Serial.println("error retrieving pressure measurement\n");
+      }
+      else Serial.println("error starting pressure measurement\n");
+    }
+    else Serial.println("error retrieving temperature measurement\n");
+  }
+  else Serial.println("error starting temperature measurement\n");
+}
+
+float getDust(void){
+  float voMeasured = 0;
+  float calcVoltage = 0;
+  float dustDensity = 0.0;
+  
+  digitalWrite(ledPower,LOW); // power on the LED
+  delayMicroseconds(280);
+ 
+  voMeasured = analogRead(measurePin); // read the dust value
+ 
+  delayMicroseconds(40);
+  digitalWrite(ledPower,LOW); // turn the LED off
+  delayMicroseconds(9680);
+ 
+  // 0 - 3.3V mapped to 0 - 1023 integer values
+  // recover voltage
+  calcVoltage = voMeasured * (3.3 / 1024);
+ 
+  // linear eqaution taken from http://www.howmuchsnow.com/arduino/airquality/
+  // Chris Nafis (c) 2012
+  dustDensity = 0.16666 * calcVoltage - 0.012;
+  return dustDensity;
+  }
