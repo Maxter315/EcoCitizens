1770fdfc83e2e1a82fd6a225c2d65e16a08feb77
Maxter315 <maxter315@gmail.com>
2016-09-14 16:10:14 +0300

    JSON encoding added

--- ard_src/ecx001/ecx001.ino
+++ ard_src/ecx001/ecx001.ino
@@ -33,16 +33,49 @@ void loop() {
 
     if (time_cur - time_prev > dt){
         //sampling & collecting
-        data[iter] = getSensorsReadings();
+        //data[iter] = getSensorsReadings();
+        //Reading ans = avgReadings(data,10);
 
         if (iter >= 4){
-            //averaging, json, send to esp
-            Reading ans = avgReadings(data,DATLEN);
+            //json, send to esp
 
             //json generating
-
-            
             /*
+            aJsonObject* amono,adust,atemp,apres,ahum;
+                amono = aJson.createArray();
+                adust = aJson.createArray();
+                atemp = aJson.createArray();
+                apres = aJson.createArray();
+                ahum = aJson.createArray();
+
+            for (int i=0;i<5;i++){
+                aJsonObject* foo;
+
+                foo = aJson.createItem(data[i].mono);
+                aJson.addItemToArray(amono, foo);
+                foo = aJson.createItem(data[i].dust);
+                aJson.addItemToArray(adust, foo);
+                foo = aJson.createItem(data[i].temp);
+                aJson.addItemToArray(atemp, foo);
+                foo = aJson.createItem(data[i].pres);
+                aJson.addItemToArray(apres, foo);
+                foo = aJson.createItem(data[i].hum);
+                aJson.addItemToArray(ahum, foo);
+            }
+            
+
+            aJson.addItemToObject(root)
+
+
+
+            aJsonObject* root = aJson.createObject();
+            aJson.addItemToObject(root, "id", aJson.createItem(SENSOR_ID));
+            aJsonObject* loc = aJson.createArray();
+
+            aJson.addItemToObject(root, "id", );
+            aJsonObject* fmt = aJson.createObject();
+            */
+            /***************************/
             StaticJsonBuffer<400> jsonBuffer;
             JsonObject& root = jsonBuffer.createObject();
 
@@ -56,11 +89,30 @@ void loop() {
             root["tsamp"] = 180;
             root["samples"] = 5;
             JsonArray& read_j = root.createNestedArray("readings");
-                JsonArray& mono_j = read_j.createNestedObject();
-                    mono_a = 
+                JsonObject& mono_o = read_j.createNestedObject();
+                JsonObject& dust_o = read_j.createNestedObject();
+                JsonObject& temp_o = read_j.createNestedObject();
+                JsonObject& pres_o = read_j.createNestedObject();
+                JsonObject& hum_o = read_j.createNestedObject();
+                
+
+            JsonArray& amono = mono_o.createNestedArray("mono");
+            JsonArray& adust = dust_o.createNestedArray("dust");
+            JsonArray& atemp = temp_o.createNestedArray("temp");
+            JsonArray& apres = pres_o.createNestedArray("pres");
+            JsonArray& ahum = hum_o.createNestedArray("hum");
+
+            for (int i=0;i<5;i++){
+                amono.add(double_with_n_digits(data[i].mono, 1));
+                adust.add(double_with_n_digits(data[i].dust, 1));
+                atemp.add(double_with_n_digits(data[i].temp, 1));
+                apres.add(double_with_n_digits(data[i].pres, 1));
+                ahum.add(double_with_n_digits(data[i].hum, 1));
+            }
 
             root.printTo(Serial);
-            Serial.println();*/
+            Serial.println();
+            /**********************************/
 
             iter = 0;
         } else {
@@ -75,20 +127,20 @@ Reading avgReadings(Reading* array, int num){
     Reading acc;
 
     for(int i=0; i<num; i++){
-        acc.CO += array[i].CO;
+        acc.mono += array[i].mono;
         acc.dust += array[i].dust;
-        acc.temperature += array[i].temperature;
-        acc.pressure += array[i].pressure;
-        acc.humidity += array[i].humidity;
+        acc.temp += array[i].temp;
+        acc.pres += array[i].pres;
+        acc.hum += array[i].hum;
     }
 
     float denom = (float)num;
 
-    acc.CO = acc.CO / denom;
+    acc.mono = acc.mono / denom;
     acc.dust = acc.dust / denom;
-    acc.temperature = acc.temperature / denom;
-    acc.pressure = acc.pressure / denom;
-    acc.humidity = acc.humidity / denom;
+    acc.temp = acc.temp / denom;
+    acc.pres = acc.pres / denom;
+    acc.hum = acc.hum / denom;
 
     return acc;
 }
